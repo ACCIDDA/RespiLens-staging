433be4db06405aa10be5c3d538e5b2ffe9f2fb9c
Emily Przykucki <emily.przykucki@gmail.com>
2025-09-29 15:58:51 -0400

    Rebuild data processing pipeline and remove first attempt
    Now process data with one script: process_RespiLens_data.py (uses classes defined in other pathogen-named files)

--- scripts/helper.py
+++ scripts/helper.py
@@ -2,8 +2,48 @@
 
 import json 
 from typing import Literal
+import numpy as np
 import pandas as pd
 from pathlib import Path
+
+
+def clean_nan_values(df: pd.DataFrame) -> pd.DataFrame:
+    """Purge dfs of JSON-incompatible `NaN` values."""
+    return df.replace({np.nan: None})
+
+
+def hubverse_df_preprocessor(df: pd.DataFrame) -> pd.DataFrame: # can add **kwargs if we need modular quantile filtering
+    """
+    Do a number of pre-processing tasks that make a hubverse df ready to pass through a processing class.
+
+    Args:
+        df: Hubverse data as pd.DataFrame
+
+    Returns:
+        A df with... 
+            - horizons as ints, 
+            - horizon NaNs dropped, 
+            - only some `output_type_id` values kept, 
+            - all `output_type` == sample removed.
+    """
+    df = df.copy()
+    # Drop NaN values in horizon column
+    df = df.dropna(subset=['horizon'])
+    # Ensure horizons are ints (not floats)
+    df['horizon'] = df['horizon'].astype(int)
+    # Get rid of all output_type == 'sample'
+    df = df[df['output_type'] != 'sample']
+    # Filter `output_type_id` values
+    # Only keep some quantiles, if pmf is implicated keep all `output_type_id` values
+    categorical_ids = ['decrease', 'increase', 'large_decrease', 'large_increase', 'stable'] 
+    numeric_ids = [0.025, 0.25, 0.5, 0.75, 0.975]
+    numeric_output_ids = pd.to_numeric(df['output_type_id'], errors='coerce')
+    is_categorical_id = df['output_type_id'].isin(categorical_ids)
+    is_numeric_id = numeric_output_ids.isin(numeric_ids)
+    df = df[is_categorical_id | is_numeric_id]
+
+    return df
+
 
 def get_location_info(
         location_data: pd.DataFrame, 
@@ -38,7 +78,8 @@ def save_json_file(
         pathogen: Literal['flu','rsv','covid'],
         output_path: str,
         output_filename: str,
-        file_contents: dict
+        file_contents: dict,
+        overwrite: bool
 ) -> None:
     """
     Save an already-validated JSON to output_path/pathogen-ext/file_name.json
@@ -69,7 +110,7 @@ def save_json_file(
     # Create dir, construct full path for the output file, fail if overwrite will occur
     target_dir.mkdir(parents=True, exist_ok=True)
     file_path = target_dir / output_filename
-    if file_path.exists():
+    if (not overwrite) and file_path.exists():
         raise FileExistsError(f"Error saving {output_filename}; file already found at {file_path}." 
                               f"Remove or move file and try again.")
     
---
+++ scripts/process_RespiLens_data.py
@@ -0,0 +1,147 @@
+"""Pull/process all data required for RespiLens"""
+
+import argparse
+import logging
+import pandas as pd
+import sys
+from pathlib import Path
+from hubdata import connect_hub
+
+
+from flusight_data_processor import FlusightDataProcessor
+from rsv_data_processor import RSVDataProcessor
+from covid19_data_processor import COVIDDataProcessor
+# from process_cdc_data import CDCDataProcessor
+from helper import save_json_file, hubverse_df_preprocessor, clean_nan_values
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """
+    Main execution function
+    """
+    parser = argparse.ArgumentParser(description = 'Pull/process all data required for RespiLens.')
+    parser.add_argument("--output-path",
+                        type=str,
+                        required=False,
+                        help="Absolute path where you want to save data do.")
+    parser.add_argument("--flusight-hub-path",
+                        type=str,
+                        required=False,
+                        help="Absolute path to local clone of FluSight forecast repo.")
+    parser.add_argument("--rsv-hub-path",
+                        type=str,
+                        required=False,
+                        help="Absolute path to local clone of RSV forecast repo.")
+    parser.add_argument("--covid-hub-path",
+                        type=str,
+                        required=False,
+                        help="Absolute path to local clone of COVID19 forecaast repo.")
+    # parser.add_argument("--CDC") ?
+    args = parser.parse_args()
+
+    if not (args.flusight_hub_path or args.rsv_hub_path or args.covid_hub_path):
+        print("🛑 No hub paths provided 🛑, so no data can be fetched.")
+        print("Please re-run script with hub path(s) specified.")
+        sys.exit(1)
+
+    logger.info("Beginning conversion process...")
+
+    if args.flusight_hub_path:
+        # Use HubdataPy to get all Flusight data in one df
+        logger.info("Establishing connection to local FluSight repository...")
+        flu_hub_conn = connect_hub(args.flusight_hub_path)
+        logger.info("Success ✅")
+        logger.info("Collecting data from FluSight repo...")
+        flu_hubverse_df = clean_nan_values(hubverse_df_preprocessor(df=flu_hub_conn.get_dataset().to_table().to_pandas()))
+        flu_locations_data = clean_nan_values(pd.read_csv(Path(args.flusight_hub_path) / 'auxiliary-data/locations.csv'))
+        flu_target_data = clean_nan_values(pd.read_csv(Path(args.flusight_hub_path) / 'target-data/time-series.csv'))
+        logger.info("Success ✅")
+        # Initialize converter object
+        flu_processor_object = FlusightDataProcessor(
+            data=flu_hubverse_df,
+            locations_data=flu_locations_data,
+            target_data=flu_target_data
+        )
+        # Iteratively save output files
+        logger.info("Saving flu JSON files...")
+        for filename, contents in flu_processor_object.output_dict.items():
+            save_json_file(
+                pathogen='flu',
+                output_path=args.output_path,
+                output_filename=filename,
+                file_contents=contents,
+                overwrite=True
+            )
+        logger.info("Success ✅")
+
+    
+    if args.rsv_hub_path:
+        # Use HubdataPy to get all RSV data in one df
+        logger.info("Establishing connection to local RSV repository...")
+        rsv_hub_conn = connect_hub(args.rsv_hub_path)
+        logger.info("Success ✅")
+        logger.info("Collecting data from RSV repo...")
+        rsv_hubverse_df = clean_nan_values(hubverse_df_preprocessor(df=rsv_hub_conn.get_dataset().to_table().to_pandas()))
+        rsv_locations_data = clean_nan_values(pd.read_csv(Path(args.rsv_hub_path) / 'auxiliary-data/location_census/locations.csv'))
+        # RSV target-data files have dynamic names based on date; find most recent one
+        rsv_base_path = Path(args.rsv_hub_path)
+        rsv_target_data_files = sorted(rsv_base_path.glob("target-data/*_rsvnet_hospitalization.csv"))
+        if not rsv_target_data_files:
+            raise FileNotFoundError("Could not find RSV target data files. Check that target-data dir exists in local RSV repo.")
+        latest_rsv_target_file_path = rsv_target_data_files[-1]
+        # Explicitly read in 'location' dtype as str b/c there are mixed values 
+        rsv_target_data = clean_nan_values(pd.read_csv(latest_rsv_target_file_path, dtype={'location': str}))
+        # Initialize converter object
+        rsv_processor_object = RSVDataProcessor(
+            data=rsv_hubverse_df,
+            locations_data=rsv_locations_data,
+            target_data=rsv_target_data
+        )
+        # Iteratively save output files
+        logger.info("Saving RSV JSON files...")
+        for filename, contents in rsv_processor_object.output_dict.items():
+            save_json_file(
+                pathogen='rsv',
+                output_path=args.output_path,
+                output_filename=filename,
+                file_contents=contents,
+                overwrite=True
+            )
+        logger.info("Success ✅")
+    
+    if args.covid_hub_path:
+        # Use HubdataPy to get all COVID data in one df
+        logger.info("Establishing connection to local COVID repository...")
+        covid_hub_conn = connect_hub(args.covid_hub_path)
+        logger.info("Success ✅")
+        logger.info("Collecting data from COVID repo...")
+        covid_hubverse_df = clean_nan_values(hubverse_df_preprocessor(df=covid_hub_conn.get_dataset().to_table().to_pandas()))
+        covid_locations_data = clean_nan_values(pd.read_csv(Path(args.covid_hub_path) / 'auxiliary-data/locations.csv'))
+        covid_target_data = clean_nan_values(pd.read_parquet(Path(args.covid_hub_path) / 'target-data/time-series.parquet'))
+        logger.info("Success ✅")
+        # Initialize converter object
+        covid_processor_object = COVIDDataProcessor(
+            data=covid_hubverse_df,
+            locations_data=covid_locations_data,
+            target_data=covid_target_data
+        )
+        # Iteratively save output files
+        logger.info("Saving COVID JSON files...")
+        for filename, contents in covid_processor_object.output_dict.items():
+            save_json_file(
+                pathogen='covid',
+                output_path=args.output_path,
+                output_filename=filename,
+                file_contents=contents,
+                overwrite=True
+            )
+    
+    logger.info("Process complete.")
+
+
+if __name__ == "__main__":
+    main()
+    
