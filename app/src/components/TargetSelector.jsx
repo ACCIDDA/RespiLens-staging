04a421f2bb983d5bd35771d2f9179cd6092978c8
Emily Przykucki <emily.przykucki@gmail.com>
2025-10-21 13:32:14 -0400

    Alias targets in long-form

--- app/src/components/TargetSelector.jsx
+++ app/src/components/TargetSelector.jsx
@@ -1,26 +1,18 @@
-// src/components/TargetSelector.jsx
 import { Select, Stack } from '@mantine/core';
 import { useView } from '../hooks/useView';
-
-const formatTargetNameForDisplay = (name) => {
-  if (!name) return '';
-  // This will format a string like "wk inc covid hosp" into "Wk Inc Covid Hosp"
-  return name.split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ');
-};
+import { targetDisplayNameMap } from '../utils/mapUtils';
 
 const TargetSelector = () => {
   // Get the target-related state and functions from our central context
   const { availableTargets, selectedTarget, handleTargetSelect } = useView();
 
   // Disable the selector if there's only one (or zero) options
-  const isDisabled = !availableTargets || availableTargets.length <= 1;
+  const isDisabled = !availableTargets || availableTargets.length < 1;
 
   // Format the targets for the Select component's `data` prop
   const selectData = availableTargets.map(target => ({
     value: target,
-    label: formatTargetNameForDisplay(target)
+    label: targetDisplayNameMap[target] || target
   }));
 
   return (
---
+++ app/src/utils/mapUtils.js
@@ -0,0 +1,9 @@
+// maps 
+
+export const targetDisplayNameMap = {
+    'wk inc covid hosp': 'Weekly Incident COVID-19 Hospitalizations',
+    'wk inc covid prop ed visits': "Proportion of ED Visits for COVID-19",
+    'wk inc flu hosp': 'Weekly Incident Flu Hospitalizations',
+    'wk inc rsv hosp': 'Weekly Incident RSV Hospitalizations',
+    'wk inc rsv prop ed visits':  'Proportion of ED Visits for RSV'
+};
