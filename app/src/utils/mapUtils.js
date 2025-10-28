150b060560898a1f84ce20d2e74a1252d23c33df
Emily Przykucki <emily.przykucki@gmail.com>
2025-10-28 13:43:04 -0400

    Update mapUtils.js

--- app/src/utils/mapUtils.js
+++ app/src/utils/mapUtils.js
@@ -2,8 +2,8 @@
 
 export const targetDisplayNameMap = {
     'wk inc covid hosp': 'Weekly Incident COVID-19 Hospitalizations',
-    'wk inc covid prop ed visits': "Proportion of ED Visits for COVID-19",
+    'wk inc covid prop ed visits': "Proportion of ED Visits due to COVID-19",
     'wk inc flu hosp': 'Weekly Incident Flu Hospitalizations',
     'wk inc rsv hosp': 'Weekly Incident RSV Hospitalizations',
-    'wk inc rsv prop ed visits':  'Proportion of ED Visits for RSV'
+    'wk inc rsv prop ed visits':  'Proportion of ED Visits due to RSV'
 };
