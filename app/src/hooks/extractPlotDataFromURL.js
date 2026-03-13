8125919a02e290e8179563f4fc7204e33c204666
Emily Przykucki <emily.przykucki@gmail.com>
2026-03-13 12:22:32 -0400

    add more detail to `plotData` object

--- app/src/hooks/extractPlotDataFromURL.js
+++ app/src/hooks/extractPlotDataFromURL.js
@@ -14,18 +14,23 @@ export const extractPlotData = (viewType, href) => {
   let fileName = "";
   let fullDataPath = "";
   let target = "";
+  let columns = [];
+  let models = [];
 
-  // Translate pseudo-code logic into JavaScript
   switch (viewType) {
     case "covid_forecasts":
       dataSuffix = "covid19";
       location = params.has("location") ? params.get("location") : "US";
       fileName = `${location}_${dataSuffix}.json`;
+      fullDataPath = `processed_data/covid19forecasthub/${fileName}`;
       target = params.has("covid_target")
         ? params.get("covid_target")
-        : "wk inc covid hosp"; // TODO: get right target
+        : "wk inc covid hosp";
+      const covidModelsString = params.get("covid_models");
+      models = covidModelsString
+        ? covidModelsString.split(",")
+        : ["CovidHub-ensemble"];
       break;
-
     case "flu_forecasts":
     case "fludetailed":
       dataSuffix = "flu";
@@ -34,37 +39,56 @@ export const extractPlotData = (viewType, href) => {
       fullDataPath = `processed_data/flusight/${fileName}`;
       target = params.has("flu_target")
         ? params.get("flu_target")
-        : "wk inc flu hosp"; // TODO: get right target
+        : "wk inc flu hosp";
+      const fluModelsString = params.get("flu_models");
+      models = fluModelsString
+        ? fluModelsString.split(",")
+        : ["FluSight-ensemble"];
       break;
 
     case "rsv_forecasts":
       dataSuffix = "rsv";
       location = params.has("location") ? params.get("location") : "US";
       fileName = `${location}_${dataSuffix}.json`;
-      fullDataPath = `processed_data/flusight/${fileName}`;
+      fullDataPath = `processed_data/rsvforecasthub/${fileName}`;
       target = params.has("rsv_target")
         ? params.get("rsv_target")
-        : "wk inc rsv hosp"; // TODO: get right target
+        : "wk inc rsv hosp";
+      const rsvModelsString = params.get("rsv_models");
+      models = rsvModelsString
+        ? rsvModelsString.split(",")
+        : ["RSVHub-ensemble"];
       break;
 
     case "metrocast_forecasts":
       dataSuffix = "flu_metrocast";
       location = params.has("location") ? params.get("location") : "colorado";
       fileName = `${location}_${dataSuffix}.json`;
-      fullDataPath = `processed_data/flusight/${fileName}`;
-      target = "pct ed visits due to flu"; // TODO: get right target
+      fullDataPath = `processed_data/flumetrocast/${fileName}`;
+      target = "Flu ED visits pct";
+      const metrocastModelsString = params.get("metrocast_models");
+      models = metrocastModelsString
+        ? metrocastModelsString.split(",")
+        : ["epiENGAGE-ensemble_mean"];
       break;
 
     case "nhsnall":
       dataSuffix = "nhsn";
       location = params.has("location") ? params.get("location") : "US";
       fileName = `${location}_${dataSuffix}.json`;
-      fullDataPath = `processed_data/flusight/${fileName}`;
-      // TODO: need to handle target (aka column+column unit combo for nhsn)
+      fullDataPath = `processed_data/nhsn/${fileName}`;
+      target = params.has("nhsn_target")
+        ? params.get("nhsn_target")
+        : "Hospital Admissions (rates)";
+      const nhsnColsFromUrl = params.getAll("nhsn_cols");
+      columns =
+        nhsnColsFromUrl.length > 0
+          ? nhsnColsFromUrl
+          : ["totalconfflunewadm", "totalconfc19newadm", "totalconfrsvnewadm"]; // TODO: slug:longform mapping
       break;
 
     default:
-      throw new Error(`Unknown view type: ${viewType}`);
+      throw new Error(`Unknown view type: ${viewType}`); // TODO: handle peak view??
   }
 
   // Construct the finalized plotData object
@@ -78,6 +102,8 @@ export const extractPlotData = (viewType, href) => {
       fileName,
       fullDataPath,
       target,
+      columns,
+      models,
     },
   };
 
